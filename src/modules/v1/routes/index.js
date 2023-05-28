cab4282aa462055f554eda34ea7fa530140556ff
radityaIch <raditya2678@gmail.com>
2023-05-28 08:23:15 +0800

    Pretify code

--- src/modules/v1/routes/index.js
+++ src/modules/v1/routes/index.js
@@ -2,9 +2,9 @@ import path from 'path';
 import { Router } from 'express';
 import multer from 'multer';
 import {
-	login,
-	refreshToken,
-	verifyToken,
+  login,
+  refreshToken,
+  verifyToken,
 } from '../controllers/auth/AuthController';
 import authenticatedMiddleware from '../middleware/authenticated';
 import { predict } from '../controllers/ModelController';
@@ -12,19 +12,19 @@ import { predict } from '../controllers/ModelController';
 const router = Router();
 
 const upload = multer({
-  	storage: multer.memoryStorage(),
+  storage: multer.memoryStorage(),
 });
 
 export const loadAuthRouter = app => {
-	router.route('/login').post(login);
+  router.route('/login').post(login);
 
-	router.use(authenticatedMiddleware);
-	router.route('/verify').get(verifyToken);
-	router.route('/refresh').post(refreshToken);
+  router.use(authenticatedMiddleware);
+  router.route('/verify').get(verifyToken);
+  router.route('/refresh').post(refreshToken);
 
-	app.use('/auth', router);
+  app.use('/auth', router);
 };
 
 export const loadModelRouter = () => {
-	router.post('/predict', upload.single('image'), predict);
+  router.post('/predict', upload.single('image'), predict);
 };
