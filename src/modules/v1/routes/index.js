e5ecf2cb583d0cf3ae17ec204789e3fbeec7a7ad
radityaIch <raditya2678@gmail.com>
2023-05-28 08:03:20 +0800

    Add model controller to router

--- src/modules/v1/routes/index.js
+++ src/modules/v1/routes/index.js
@@ -1,17 +1,30 @@
-import path from 'path'
-import { Router } from 'express'
-import multer from 'multer'
-import { login, refreshToken, verifyToken } from '../controllers/auth/AuthController'
-import authenticatedMiddleware from '../middleware/authenticated'
+import path from 'path';
+import { Router } from 'express';
+import multer from 'multer';
+import {
+	login,
+	refreshToken,
+	verifyToken,
+} from '../controllers/auth/AuthController';
+import authenticatedMiddleware from '../middleware/authenticated';
+import { predict } from '../controllers/ModelController';
+
+const router = Router();
+
+const upload = multer({
+  	storage: multer.memoryStorage(),
+});
 
 export const loadAuthRouter = app => {
-    const router = Router()
+	router.route('/login').post(login);
 
-    router.route('/login').post(login)
+	router.use(authenticatedMiddleware);
+	router.route('/verify').get(verifyToken);
+	router.route('/refresh').post(refreshToken);
 
-    router.use(authenticatedMiddleware)
-    router.route('/verify').get(verifyToken)
-    router.route('/refresh').post(refreshToken)
+	app.use('/auth', router);
+};
 
-    app.use('/auth', router)
-}
+export const loadModelRouter = () => {
+	router.post('/predict', upload.single('image'), predict);
+};
